ed1329a62ef5b7a14296975f066df4712e56bf36
ouafae-boop <ouafaeamoumri@gmail.com>
2025-11-25 00:43:57 +0100

    message

--- Day_01/print_alphabet.c
+++ Day_01/print_alphabet.c
@@ -1,12 +1,14 @@
 #include<stdio.h>
 #include<unistd.h>
-void printf_alphabet(){
+void printf_alphabet(void){
     char ch;
     for(ch='a'; ch<='z'; ch++){
         write(1, &ch, 1);
     }
-    int main() {
-        pintf_alphabet();
-        return 0;
-    }
 }
+int main() {
+        printf_alphabet();
+        return 0;
+}
+
+
